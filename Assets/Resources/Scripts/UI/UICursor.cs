f82504cbc6944e7e810db17741cab9f4391e5092
dMorenov <danielmorenotvjb42013@cev.com>
2015-05-05 19:06:39 +0200

    zxcxc

---
+++ Assets/Resources/Scripts/UI/UICursor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UICursor : MonoBehaviour {
+	public Texture2D cursorTexture;
+	public CursorMode cursorMode = CursorMode.ForceSoftware;
+	public Vector2 hotSpot = Vector2.zero;
+
+
+	public void Awake()
+	{
+		Cursor.SetCursor (cursorTexture, Vector2.zero, cursorMode);
+	}
+
+}
